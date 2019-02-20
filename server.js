3a5a93b47684598b0f868f08fc1fa68201d658fa
Mark Tovey <mtovey@ionagroup.com>
2019-02-20 12:43:49 -0600

    Add a description.

--- server.js
+++ server.js
@@ -27,7 +27,7 @@ app.post('/', function (req, res) {
       if(weather.main == undefined){
         res.render('index', {weather: null, error: 'Error, please try again'});
       } else {
-        let weatherText = `It's ${weather.main.temp} degrees in ${weather.name}!`;
+        let weatherText = `It's ${weather.main.temp} degrees in ${weather.name} ${weather.weather[0].description}!`;
         res.render('index', {weather: weatherText, error: null});
       }
     }
